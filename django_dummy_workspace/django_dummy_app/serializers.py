1b01a5e2284c00bff81c949227ef386cae980625
BryceeL <bcllamc280@gmail.com>
2024-11-03 19:52:41 -0800

    uses the newly named model 'user_messages'

--- django_dummy_workspace/django_dummy_app/serializers.py
+++ django_dummy_workspace/django_dummy_app/serializers.py
@@ -1,8 +1,8 @@
 # serializers.py
 from rest_framework import serializers
-from .models import messages
+from .models import user_messages
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = messages
+        model = user_messages
         fields = ('message_id', 'reciever_id', 'text', 'user_id')
