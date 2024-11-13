e8801588e72ce165390c3f4d600a398e1242bc80
Elias Alvarez <elias.alvarez880@gmail.com>
2024-11-13 00:13:25 -0800

    posts unit tests

--- django_dummy_workspace/django_dummy_app/models.py
+++ django_dummy_workspace/django_dummy_app/models.py
@@ -6,21 +6,12 @@
 
 # Create your models here.
 
-<<<<<<< HEAD
-class likes(models.Model):
-    id = models.AutoField(primary_key=True, null=False)
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45)
-    comment = models.CharField(max_length=45)
-    like_count = models.IntegerField()
-=======
 # def generate_message_id():
 #     while True:
 #         id = uuid.uuid4()
 #         if user_messages.objects.filter(message_id=id).count() == 0:
 #             break
 #     return id
->>>>>>> origin
 
 # def convert_pst(time):
 #     if time.tzinfo is None:
@@ -39,16 +30,6 @@ class likes(models.Model):
 #     def __str__(self):
 #         return self.name
     
-<<<<<<< HEAD
-class users(models.Model):                                         
-    user_id = models.AutoField(primary_key=True, null=False)
-    username = models.CharField(max_length=15)
-    password = models.CharField(max_length=45)
-    status = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-    follower_id = models.IntegerField(default=0)
-=======
 # class users(models.Model):                                         
 #     user_id = models.IntegerField(primary_key=True, unique=True, null=False)
 #     username = models.CharField(max_length=15)
@@ -57,33 +38,17 @@ class users(models.Model):
 #     first_name = models.CharField(max_length=45)
 #     last_name = models.CharField(max_length=45)
 #     follower_id = models.IntegerField()
->>>>>>> origin
 
 #     def str(self):
 #         return self.user_id
 
-<<<<<<< HEAD
-class personal_pages(models.Model):
-    page_id = models.AutoField(primary_key=True, null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-=======
 # class personal_pages(models.Model):
 #     page_id = models.IntegerField(primary_key=True, null=False)
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
->>>>>>> origin
 
 #     def str(self):
 #         return self.page_id
 
-<<<<<<< HEAD
-class posts(models.Model):
-    post_id = models.AutoField(primary_key=True, null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    media = models.CharField(max_length=255)
-    text = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
-=======
 # class posts(models.Model):
 #     post_id = models.IntegerField(primary_key=True, null=False)
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -92,19 +57,12 @@ class posts(models.Model):
 #     title = models.CharField(max_length=255)
 #     username = models.CharField(max_length=255)
 #     creation_date = models.DateTimeField(auto_now_add=True)
->>>>>>> origin
 
 #     def str(self):
 #         return self.post_id
 #     def pst_creation_date(self):
 #         return convert_pst(self.creation_date)
 
-<<<<<<< HEAD
-class comments(models.Model):
-    comment_id = models.AutoField(primary_key=True, null=False)
-    post = models.ForeignKey(posts, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-=======
 # class comments(models.Model):
 #     comment_id = models.IntegerField(primary_key=True, null=False, default=-1)
 #     user_id = models.IntegerField(null=False)
@@ -112,24 +70,17 @@ class comments(models.Model):
 #     comment = models.CharField(max_length=255)
 #     creation_date = models.DateTimeField(auto_now_add=True)
 #     # post = models.ForeignKey(posts, on_delete=models.CASCADE)
->>>>>>> origin
 
 #     def str(self):
 #         return self.comment_id
 #     def pst_creation_date(self):
 #         return convert_pst(self.creation_date)
 
-<<<<<<< HEAD
-class replies(models.Model):
-    reply_id = models.AutoField(primary_key=True, null=False)
-    text = models.CharField(max_length=255)
-=======
 # class replies(models.Model):
 #     reply_id = models.IntegerField(primary_key=True, null=False)
 #     user_id = models.IntegerField(null=False)
 #     comment_id = models.IntegerField(null=False)
 #     reply = models.CharField(max_length=255)
->>>>>>> origin
 
 #     def str(self):
 #         return self.reply_id
@@ -146,17 +97,10 @@ class replies(models.Model):
 #     def pst_creation_date(self):
 #         return convert_pst(self.creation_date)
     
-<<<<<<< HEAD
-class follow(models.Model):
-    primary_key = models.AutoField(primary_key=True, null = False)
-    follower_id = models.IntegerField(null=False)
-    followee_id = models.IntegerField(null=False)
-=======
 # class follow(models.Model):
 #     primary_key = models.IntegerField(primary_key=True, null = False)
 #     follower_id = models.IntegerField(null=False)
 #     followee_id = models.IntegerField(null=False)
->>>>>>> origin
 
 #     def str(self):
 #         return self.follow_id
--- django_dummy_workspace/posts/tests.py
+++ django_dummy_workspace/posts/tests.py
@@ -1,3 +1,169 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import posts, users, comments, replies
 
-# Create your tests here.
+class PostViewTests(APITestCase):
+    
+    def setUp(self):
+        # Create a test user and post
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(author=self.user, title="Test Post", description="Test Desc", media="media")
+
+    def test_get_post_not_found(self):
+        # Use an invalid ID that doesn't exist
+        url = reverse('post')  # replace with the name of your URL pattern
+        response = self.client.get(url, {'post_id': 99999})  # 99999 doesn't exist
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_get_post_success(self):
+        url = reverse('post')  # Adjust this to match the actual URL name
+        response = self.client.get(url, {'post_id': self.post.post_id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['post_id'], self.post.post_id)
+
+    def test_create_post_success(self):
+        url = reverse('post')  # Adjust this to match the actual URL name
+        data = {
+            'user_id': self.user.user_id,
+            'title': 'Test Post',
+            'description': 'Test Description',
+            'media': 'media_url'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['Response'], "Post was created")
+
+
+class GetPostsViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        posts.objects.create(author=self.user, title="Post 1", description="Description 1", media="media1")
+        posts.objects.create(author=self.user, title="Post 2", description="Description 2", media="media2")
+
+    def test_get_all_posts(self):
+        url = reverse('all_posts')  # replace with the name of your URL pattern
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+class GetRepliesViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(author=self.user, title="Test Post", description="Test Desc", media="media")
+        # Create a reply with the correct fields based on your model
+        replies.objects.create(author=self.user, comment_id=1, reply="Test reply")
+
+    def test_get_replies(self):
+        url = reverse('replies')  # replace with the name of your URL pattern
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+
+
+class CommentsViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(post_id=1, author=self.user, title="Test Post", description="Test Desc", media="media")
+        comments.objects.create(author=self.user, post=self.post, comment="This is a test comment")
+
+    def test_get_comments_success(self):
+        url = reverse('comments')  # replace with the name of your URL pattern
+        response = self.client.get(url, {'post_id': self.post.post_id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.json()), 0)
+
+    def test_create_comment_success(self):
+        url = reverse('comments')
+        data = {
+            "user_id": self.user.user_id,
+            "post_id": self.post.post_id,
+            "comment": "A new test comment"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('Response'), "Commented created")
+###############################################################################################################################################
+
+class CreatePostViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.url = reverse('post')  # replace with your actual URL name for this endpoint
+
+    def test_create_post(self):
+        data = {
+            'user_id': self.user.user_id,
+            'title': 'New Post Title',
+            'description': 'New post description',
+            'media': 'media_url'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['Response'], "Post was created")
+
+class GetPostViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(author=self.user, title="Test Post", description="Test Desc", media="media")
+        self.url = reverse('post')  # replace with your actual URL name for this endpoint
+
+    def test_get_post(self):
+        response = self.client.get(f'{self.url}?post_id={self.post.post_id}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['post_id'], self.post.post_id)
+        self.assertEqual(response.json()['title'], self.post.title)
+
+class GetAllPostsViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.posts = [
+            posts.objects.create(author=self.user, title=f"Post {i}", description=f"Description {i}", media="media_url") 
+            for i in range(3)
+        ]
+        self.url = reverse('all_posts')  # replace with your actual URL name for this endpoint
+
+    def test_get_all_posts(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+class CreateCommentViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(author=self.user, title="Test Post", description="Test Desc", media="media")
+        self.url = reverse('comments')  # replace with your actual URL name for this endpoint
+
+    def test_create_comment(self):
+        data = {
+            'user_id': self.user.user_id,
+            'post_id': self.post.post_id,
+            'comment': 'This is a test comment'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['Response'], "Commented created")
+
+class GetCommentsViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.post = posts.objects.create(author=self.user, title="Test Post", description="Test Desc", media="media")
+        self.comment = comments.objects.create(author=self.user, post=self.post, comment="Test comment")
+        self.url = reverse('comments')  # replace with your actual URL name for this endpoint
+
+    def test_get_comments(self):
+        response = self.client.get(f'{self.url}?post_id={self.post.post_id}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+        self.assertEqual(response.data[0]['comment'], self.comment.comment)
+
+class GetPostInvalidIDViewTests(APITestCase):
+    def setUp(self):
+        self.user = users.objects.create(user_id=1, username="testuser")
+        self.url = reverse('post')  # replace with your actual URL name for this endpoint
+
+    def test_get_post_invalid_id(self):
+        response = self.client.get(f'{self.url}?post_id=9999')  # Assuming 9999 doesn't exist
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['response'], "Post with this ID cannot be found")
