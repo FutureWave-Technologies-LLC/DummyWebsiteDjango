e63609d0782c83ec4ed7c86c461aa46b1ef9741c
BryceeL <bcllamc280@gmail.com>
2024-11-22 11:37:51 -0800

    compare tokens url

--- django_dummy_workspace/users/urls.py
+++ django_dummy_workspace/users/urls.py
@@ -4,12 +4,15 @@ from .views import *
 urlpatterns = [
     path('users/', get_users, name = 'user'),
     path('get_user_data/', get_user_data, name = 'get_user_data'),
-    path('authenticate_user/', authenticate_user, name='users'),
     path('search_users/', search_users, name = 'searching_users'),
 
     #LOGIN/SIGN-UP
     path('login/', login_user, name = 'login'),
     path('sign_up/', signup_user, name = 'sign_up'),
+
+    #AUTHENTICATING
+    path('authenticate_user/', authenticate_user, name='authenticate'),
+    path('compare_tokens/', compare_token_ids, name='compare token'),
 
     path('update_settings/', update_settings, name = 'settings')
 ]
