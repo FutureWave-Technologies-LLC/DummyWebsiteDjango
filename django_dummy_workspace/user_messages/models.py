0484f2b7d170d1f644dc8ec284311d956944fc67
BryceeL <bcllamc280@gmail.com>
2024-12-06 13:26:33 -0800

    readded message_id field to model

--- django_dummy_workspace/user_messages/models.py
+++ django_dummy_workspace/user_messages/models.py
@@ -1,3 +1,13 @@
+from django.db import models
+from users.models import *
+from random import randrange
+
+def generate_message_id():
+    while True:
+        id = randrange(0, 9999999)
+        if user_messages.objects.filter(message_id = id).count() == 0:
+            break
+    return id
 '''
 from django.db import models
 from users.models import *
@@ -39,6 +49,10 @@ from django.db import models
 from users.models import users
 
 class user_messages(models.Model):
+    message_id = models.IntegerField(primary_key=True,
+                                      null=False, 
+                                      unique=True,
+                                      default=generate_message_id)
     sender = models.ForeignKey(users, on_delete=models.CASCADE, related_name='sent_messages')
     receiver_id = models.IntegerField()
     message_text = models.TextField()
