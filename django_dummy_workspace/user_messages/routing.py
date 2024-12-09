7e336f92ede0d39e6007ac2b47ce338bb542a816
BryceeL <bcllamc280@gmail.com>
2024-12-08 22:46:51 -0800

    changed parameter name in websocket url
    user_id -> sender_id

--- django_dummy_workspace/user_messages/routing.py
+++ django_dummy_workspace/user_messages/routing.py
@@ -1,15 +1,15 @@
-'''
-from django.urls import re_path
-from . import consumers
+# '''
+# from django.urls import re_path
+# from . import consumers
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
-'''
+# websocket_urlpatterns = [
+#     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+# ]
+# '''
 
 from django.urls import re_path
 from user_messages.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<user_id>\d+)/(?P<receiver_id>\d+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<sender_id>\d+)/(?P<receiver_id>\d+)/$', ChatConsumer.as_asgi()),
 ]
