216b98ab9634cc0232ed3a20d75147c1463673b1
Elias Alvarez <elias.alvarez880@gmail.com>
2024-12-08 22:50:05 -0800

    serializer fix

--- django_dummy_workspace/user_messages/serializers.py
+++ django_dummy_workspace/user_messages/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import user_messages
+from .models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_id = serializers.IntegerField(source='sender.id')  
+    receiver_id = serializers.IntegerField(source='receiver.id')  
+
     class Meta:
-        model = user_messages
-        fields = ('message_id', 'sender', 'receiver_id', 'message_text')
+        model = Message
+        fields = ['sender_id', 'receiver_id', 'message_text']
